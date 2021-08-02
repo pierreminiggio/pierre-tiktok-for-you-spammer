07f134246700176f1edbcfe99e38b462abeca999
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-08-02 09:04:30 +0200

    moar scrolling

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ import fs from 'fs'
 
 fs.readFile('./ids.json', 'utf-8', (err, data) => {
     const ids = JSON.parse(data)
-    spamTikTok(
         ids.login,
         ids.password,
         ids.api,
         ids.token,
-        10000,
+        20000,
         6000,
         ids.proxy,
     ).then(() => console.log('Done !'))
